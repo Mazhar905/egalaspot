440f945233a49172429c0222744bf5b2b4076a01
hamza <hamza786504@gmail.com>
2024-07-07 04:50:27 +0500

    images url registery in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['cdn.dummyjson.com' , "res.cloudinary.com","asset.cloudinary.com"], // Add the correct hostname here
+    domains: ["res.cloudinary.com","www.alphabroder.com"], // Add the correct hostname here
   },
 }
